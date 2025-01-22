71dba2d9c7c2aab1fd95ec1e7ba2b326a0fa2f72
AndersonVanegas <106352070+AndersonVanegas@users.noreply.github.com>
2025-01-22 11:34:04 -0500

    Add files via upload

--- src/script/script.js
+++ src/script/script.js
@@ -4,18 +4,22 @@ $(document).ready(()=>{
         message = message.toLowerCase();
         checkString();
         if(message === "") {
+            const numberWithPoints = resultado.toLocaleString().replace(/,/g, '.');
             $('#salida').removeClass('d-none');
             $('#salida').addClass('d-block');
-            const numberWithPoints = resultado.toLocaleString().replace(/,/g, '.');
             $('#salida').html(`${numberWithPoints}`);
             $('.alert').removeClass('d-block');
             $('.alert').addClass('d-none');
         }else if (/[0-9]/.test(message)){
+            $('#salida').removeClass('d-block');
+            $('#salida').addClass('d-none');
             let arr = message.split(" ");
             $('.alert').removeClass('d-none');
             $('.alert').addClass('d-block');
             $('.alert').html(`No numbers please.  <b>${arr[0]}</b>`);
         }else {
+            $('#salida').removeClass('d-block');
+            $('#salida').addClass('d-none');
             let arr = message.split(" ");
             $('.alert').removeClass('d-none');
             $('.alert').addClass('d-block');
